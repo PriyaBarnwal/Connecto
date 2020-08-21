ce1f4541910e29b58ddf32f09cc314161cd0dbb4
Priyanka Barnwal <priya.barnwal@gmail.com>
2020-08-22 03:36:49 +0530

    home page, login, signup ui done

--- client/src/components/HomeContainer.js
+++ client/src/components/HomeContainer.js
@@ -1,18 +1,85 @@
 import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Carousel } from 'react-bootstrap'
+import carousel3 from '../img/carousel-3.jpg'
+import carousel4 from '../img/carousel-4.jpg'
+import carousel1 from '../img/carousel-1.png'
+import carousel2 from '../img/carousel-2.jpg'
+import carousel5 from '../img/carousel-5.jpg'
 
-const HomeContainer = (auth) => {
-  if(auth.isAuthenticated) 
+const HomeContainer = ({auth}) => {
+  if(auth.auth && auth.auth.isAuthenticated) 
     return <Redirect to='/dashboard'/>
 
   return (
-    <section>
-      <p>Welcome to Connecto</p>
-      <p>A community where you can connect with other developers, share posts and also create your portfolio.</p>
-      <div className="btn-group" role="group">
-        <Link to="/register" className="btn btn-outline-secondary btn-lg" role="button" >Sign up</Link>
-        <Link to="/login" className="btn btn-outline-secondary btn-lg" role="button">Login</Link>
+    <section className="landing-page">
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carousel1}
+            height="550"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            {/* <Badge variant="warning"><h3>CONNECT</h3></Badge> */}
+            <p>Connect with developers from all across the world</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carousel2}
+            height="550"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            {/* <Badge variant="warning"><h3>SHARE</h3></Badge> */}
+            <p>Share your ideas with others through your posts</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carousel3}
+            alt="Third slide"
+            height="550"
+          />
+          <Carousel.Caption>
+            {/* <Badge variant="warning"><h3>DISCUSS</h3></Badge> */}
+            <p>Discuss with other developers to work on your ideas.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carousel4}
+            alt="Fourth slide"
+            height="550"
+          />
+          <Carousel.Caption>
+          {/* <Badge variant="warning"><h3>LEARN</h3></Badge> */}
+            <p>Learn from others and get motivated to enhance skills.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carousel5}
+            alt="Fourth slide"
+            height="550"
+          />
+          <Carousel.Caption>
+          {/* <Badge variant="warning"><h3>CREATE</h3></Badge> */}
+            <p>Create your dream portfolio.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div className="welcome">
+        <h2>Welcome to devWEavers</h2>
+        <p>A community where you can connect with other developers, share posts, learn, grow as a developer and also create your portfolio.</p>
+        <Link to="/register" className="btn btn-outline-info btn-lg" role="button" >Get Started</Link>
       </div>
     </section>
   )
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,9 @@
-import React, {Fragment, useState} from 'react'
+import React, {useState} from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { login } from '../actions/authActions'
+import { Container } from 'react-bootstrap'
+import logo from '../img/logo.png'
 
 const Login = ({isAuthenticated, login}) => {
   const [formData, setFormData] = useState({
@@ -25,22 +27,24 @@ const Login = ({isAuthenticated, login}) => {
     return <Redirect to="/dashboard"/>
     
   return (
-    <Fragment>
-      <form className="container form-signup" onSubmit ={e=>submitForm(e)}>
-        <div className="text-center mb-3">
-          <img className="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"/>
-          <p className="lead"><i className="fas fa-user"></i> Sign in to Your Account</p>
-        </div>
-        <div className="mb-3">
-          <input type="email" id="email" value={email} className="form-control" placeholder="Email address" onChange={e=>onChange(e)} required autoFocus/>
-        </div>
-        <div>
-          <input type="password" id="password" value={password} className="form-control" placeholder="Password" onChange={e=>onChange(e)} required/>
-        </div>
-        <button className="btn btn-lg btn-primary btn-block mt-3" type="submit">Sign in</button>
-        <p className="my-1">Don't have an account? <Link to="/register">Register</Link> here</p>
-      </form>
-    </Fragment>
+    <section className="back-page">
+      <Container className="loginform-container">
+        <form className="form-signup" onSubmit ={e=>submitForm(e)}>
+          <div className="text-center mb-3">
+            <img className="mb-4" src={logo} alt="logo" height="90"/>
+            <p className="lead"><i className="fas fa-user"></i> Sign in to Your Account</p>
+          </div>
+          <div className="mb-3">
+            <input type="email" id="email" value={email} className="form-control" placeholder="Email address" onChange={e=>onChange(e)} required autoFocus/>
+          </div>
+          <div>
+            <input type="password" id="password" value={password} className="form-control" placeholder="Password" onChange={e=>onChange(e)} required/>
+          </div>
+          <button className="btn btn-lg btn-info btn-block mt-3" type="submit">Sign in</button>
+          <p className="my-1">Don't have an account? <Link to="/register" className="color-prime">Register</Link> here</p>
+        </form>
+      </Container>
+    </section>
   )
 }
 
